d7a14a0fa7290e902167498b634e946fb439b30d
vinhnguyen1801 <zinny963@gmail.com>
2024-02-21 11:57:29 +0700

    search for user front

--- FrontEndWithAjaxDemo/src/main/resources/static/js/user.js
+++ FrontEndWithAjaxDemo/src/main/resources/static/js/user.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#btn-filter-user").on("click", function () {
-        //TO DO
+        addKeywordAndSearch();
     })
     $("#btn-add-user").on("click", function () {
-        let roleId = $("#user-type-input").val();
+        let role = $("#user-type-input").val();
         let name = $("#name-input").val(); // ten that
-        let username = $("#user-name-input").val(); // ten that
+        let username = $("#user-name-input").val();
         let email = $("#email-address-input").val();
         // let password = $("#password-input").val();
         let phone = $("#phone-input").val();
         let dob = $("#dob-input").val();
         let gender = $("#gender-input").val();
         let data = {
-            roleId: "ADMIN",
+            role: role,
             name: name,
-            username: email,
+            username: username,
             password: '123456789',
             email: email,
             phone: phone,
@@ -27,12 +27,77 @@ $(document).ready(function(){
     $(".btn-update-user").on("click", function () {
         //TO DO
     })
-
-    document.querySelectorAll('.type').forEach(function(type) {
-        if (type.textContent.trim() === 'TRAINER') {
-            type.classList.add('type-trainee');
-        } else {
-            type.classList.add('type-admin');
+    $(document).on("keypress", function (e) {
+        if(e.keyCode === 13) {
+            addKeywordAndSearch();
         }
-    });
+    })
+
+    // init filter
+    const urlParams = new URLSearchParams(window.location.search);
+    let keywords = urlParams.get("keywords") ? urlParams.get("keywords").split(",") : [];
+    if (keywords && keywords.length > 0) {
+        searchKeywords = keywords;
+    }
+    renderKeywords();
 });
+
+var searchKeywords = [];
+
+function addKeywordAndSearch() {
+    var input = $("#input-filter").val();
+    if (searchKeywords.length === 4) {
+        searchKeywords.shift();
+    }
+    if (input) {
+        searchKeywords.push(input);
+        search();
+    }
+}
+
+function removeKeywordAndSearch(index) {
+    searchKeywords.splice(index, 1);
+    search();
+}
+
+function search() {
+    // Gửi yêu cầu tìm kiếm đến server
+    if (searchKeywords.length > 0) {
+        window.location.href = `/user/list?keywords=${encodeURIComponent(searchKeywords.join(','))}`;
+    } else {
+        window.location.href = `/user/list`;
+    }
+}
+
+function renderKeywords() {
+    let groupFilerDom = $("#group-filter");
+    groupFilerDom.html("");
+    for (const [index, item] of searchKeywords.entries()) {
+        let itemFilterDom = $("<span>")
+            .addClass("item-filter")
+            .addClass("me-2");
+        let keywordsContainerDom = $("<div>")
+            .html(item)
+            .addClass("me-2");
+        let btnRemoveDom = $("<div>");
+        btnRemoveDom
+            .html("X")
+            .addClass("btn-x")
+            .on("click", function () {
+                removeFilter(index);
+            });
+
+        itemFilterDom
+            .append(keywordsContainerDom)
+            .append(btnRemoveDom);
+
+        groupFilerDom
+            .append(itemFilterDom);
+    }
+}
+
+function removeFilter(index) {
+    searchKeywords.splice(index, 1);
+    renderKeywords();
+    search();
+}
